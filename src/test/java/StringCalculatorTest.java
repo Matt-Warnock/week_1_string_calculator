863233514f9f430da6e8f15d94c3c77039132306
MattW <mattnwarnock@yahoo.co.uk>
2022-09-05 16:23:44 +0100

    Refactor: praramtized tests for add 2 numbers

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -16,25 +16,11 @@ class StringCalculatorTest {
     void empty_string_returns_0() {
         assertEquals(0, new StringCalculator().add(""));
     }
-    @Test
-    void add_1_and_2_together() {
-        assertEquals(3, new StringCalculator().add("1,2"));
-    }
 
-    @Test
-    void add_4_and_7_together() {
-        assertEquals(11, new StringCalculator().add("4,7"));
+    @ParameterizedTest
+    @CsvSource({"'1,2',3", "'4,7',11", "'2,4',6"})
+    void add_two_numbers_together(String input, int Output) {
+        assertEquals(Output, new StringCalculator().add(input));
     }
-
-    @Test
-    void add_2_and_4_together() {
-        assertEquals(6, new StringCalculator().add("2,4"));
-    }
-
-//    @ParameterizedTest
-//    @CsvSource({"1,2,3", "4,7,11", "2,4,6"})
-//    void add_two_numbers_together(String input1, String input2, int Output) {
-//        assertEquals(Output, new StringCalculator().add(input1 + "," + input2));
-//    }
 
 }
